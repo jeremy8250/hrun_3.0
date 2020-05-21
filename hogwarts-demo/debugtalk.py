14543d197f76261bd988628b4a6f647675738bcc
jeremy8250 <jeremy8250@gmail.com>
2020-05-21 11:18:46 +0800

    add login and create flow

--- hogwarts-demo/debugtalk.py
+++ hogwarts-demo/debugtalk.py
@@ -1,5 +1,8 @@
+import re
 import time
 
+import requests
+from bs4 import BeautifulSoup
 from httprunner import __version__
 
 
@@ -7,9 +10,12 @@ def get_httprunner_version():
     return __version__
 
 
-def sum_two(m, n):
-    return m + n
-
-
 def sleep(n_secs):
     time.sleep(n_secs)
+
+
+def get_chartId(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    pattern = re.compile(r'var chartId = "(.*?)";$')
+    script = soup.find("script", text=pattern)
+    print(pattern.search(script.text).group(1))
